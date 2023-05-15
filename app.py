2a014d0fe652aefc0761f338d115ed7f70dc70e3
SyntxSultan <132599542+SyntxSultan@users.noreply.github.com>
2023-05-15 17:51:26 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,28 +1,24 @@
 from flask import Flask, request, jsonify, render_template
-
-# Create the app object
-app = Flask(__name__)
-
-
-# importing function for calculations
 from basic_calculator_function import basic_calculator
 
-# Define calculator
-@app.route('/')
-def home():
-    return render_template('index.html')
+def create_app():
+    # Create the app object
+    app = Flask(__name__)
 
-@app.route('/predict',methods=['POST'])
-def predict():
+    # Define calculator
+    @app.route('/')
+    def home():
+        return render_template('index.html')
 
-    a = request.form['a']
-    b = request.form['b']
-    operation = str(request.form['operation'])
+    @app.route('/predict',methods=['POST'])
+    def predict():
 
-    result = basic_calculator(a,b,operation)
+        a = request.form['a']
+        b = request.form['b']
+        operation = str(request.form['operation'])
 
-    return render_template('index.html', prediction_text=str(result))
+        result = basic_calculator(a,b,operation)
 
+        return render_template('index.html', prediction_text=str(result))
+    return app
 
-if __name__ == "__main__":
-    app.run(debug=True)
